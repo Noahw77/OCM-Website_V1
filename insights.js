40a2ae5a6548e593921ebb29f62dda6da7f42d52
Noahw77 <noah.weinstein1@gmail.com>
2025-07-18 13:57:06 -0700

    Dynamic blog

---
+++ insights.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("blog.json")
+    .then(response => response.json())
+    .then(data => {
+      const sorted = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+      const recentPosts = sorted.slice(0, 4);
+      const container = document.querySelector(".blog-grid");
+      container.innerHTML = "";
+
+      recentPosts.forEach(post => {
+        const card = document.createElement("div");
+        card.className = "blog-card";
+        card.innerHTML = `
+          <h4>${post.title}</h4>
+          <p><em>${new Date(post.date).toLocaleDateString()}</em></p>
+          <p>${post.excerpt}</p>
+          <a href="${post.link}">Read More</a>
+        `;
+        container.appendChild(card);
+      });
+
+      const archiveLink = document.createElement("p");
+      archiveLink.innerHTML = `<a href="blog-archive.html">View All Posts</a>`;
+      container.appendChild(archiveLink);
+    });
+});
